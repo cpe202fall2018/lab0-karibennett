29924330ac23ccf1099b8d3e3f5f13768fc4960f
Kari Bennett <lightning9054@gmail.com>
2018-09-24 11:27:30 -0700

    planets: complete lab assignment

--- planets.py
+++ planets.py
@@ -1,6 +1,13 @@
 def weight_on_planets():
-   # write your code here
-   
+    message = "What do you weigh on earth? "
+    earth_weight = int(input(message))
+    mars_scalar = .38
+    jupiter_scalar = 2.34
+    mars_weight = earth_weight * mars_scalar
+    jupiter_weight = earth_weight * jupiter_scalar
+    fmt_stmt = "On {} you would weigh {} pounds."
+    print("\n" + fmt_stmt.format("Mars", mars_weight) + "\n" + 
+        (fmt_stmt.format("Jupiter", jupiter_weight)))   
    
    
 if __name__ == '__main__':
